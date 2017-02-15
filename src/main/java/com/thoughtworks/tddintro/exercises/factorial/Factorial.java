e23070a1fc50700edea257e9511d55ee166962ea
marco17851 <marco17851@gmail.com>
2017-02-15 02:24:22 -0500

    Adding factorial changes to compute

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,14 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i) throws IllegalArgumentException{
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (i == 0 || i == 1){
+            return 1;
+        }
+        return i * compute(i-1);
     }
 }
